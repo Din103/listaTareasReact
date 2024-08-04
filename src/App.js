6f4d738b42eac482cc4a11f87946eabfb7f1d79e
Din103 <xbeorlegi@gmail.com>
2024-08-04 23:19:56 +0200

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+//App.css
+
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+// import ListaDeTareas from './componentes/ListaDeTareas';
+import Tarea from './componentes/Tarea';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='aplicacion-tareas'>
+      <div className='freecodecamp-logo-contenedor'>
+        {/* <img 
+          src={freeCodeCampLogo} 
+          className='freecodecamp-logo' alt="kkk" /> */}
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mi Lista de Tareas Pendientes</h1>
+        <Tarea texto='Aprender React'/>
+        {/* <ListaDeTareas /> */}
+      </div>
     </div>
   );
 }
